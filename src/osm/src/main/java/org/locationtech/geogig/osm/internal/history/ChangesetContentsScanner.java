760e2ca27c9f8e8766ba818f25526f362ddf805c
Jody Garnett <jody.garnett@gmail.com>
2014-09-04 00:26:22 -0700

    update headers with header generated from git history

--- src/osm/src/main/java/org/locationtech/geogig/osm/internal/history/ChangesetContentsScanner.java
+++ src/osm/src/main/java/org/locationtech/geogig/osm/internal/history/ChangesetContentsScanner.java
@@ -1,10 +1,12 @@
-/*******************************************************************************
- * Copyright (c) 2013 Boundless and others.
+/* Copyright (c) 2013 Boundless and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Distribution License v1.0
  * which accompanies this distribution, and is available at
  * https://www.eclipse.org/org/documents/edl-v10.html
- *******************************************************************************/
+ *
+ * Contributors:
+ * Victor Olaya (Boundless) - initial implementation
+ */
 package org.locationtech.geogig.osm.internal.history;
 
 import static com.google.common.base.Optional.fromNullable;

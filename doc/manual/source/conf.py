67a9e54512ea9a91849e556dc5fbe90877b35d77
Jody Garnett <jody.garnett@gmail.com>
2016-11-21 16:16:20 -0800

    Add license information to documentation

--- doc/manual/source/conf.py
+++ doc/manual/source/conf.py
@@ -42,7 +42,7 @@ master_doc = 'index'
 # General information about the project.
 project = u'GeoGig'
 manual = u'User Manual'
-copyright = u'2013, OpenGeo'
+copyright = u'2016, OpenGeo'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the

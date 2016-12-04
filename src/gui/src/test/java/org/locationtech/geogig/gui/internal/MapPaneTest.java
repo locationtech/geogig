bbb038f06d00e643145af2e7e0c4c2b7cdd376e5
Gabriel Roldan <groldan@boundlessgeo.com>
2016-12-04 19:55:59 -0300

    Add experimental gui module
    currently only contains a very basic map
    browser based off the geotools demo MapPane
    
    Signed-off-by: Gabriel Roldan <groldan@boundlessgeo.com>

---
+++ src/gui/src/test/java/org/locationtech/geogig/gui/internal/MapPaneTest.java
@@ -0,0 +1,38 @@
+/* Copyright (c) 2016 Boundless and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Distribution License v1.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/org/documents/edl-v10.html
+ *
+ * Contributors:
+ * Gabriel Roldan (Boundless) - initial implementation
+ */
+package org.locationtech.geogig.gui.internal;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.locationtech.geogig.gui.internal.MapPane;
+import org.locationtech.geogig.repository.GeoGIG;
+import org.locationtech.geogig.repository.Repository;
+import org.locationtech.geogig.test.integration.RepositoryTestCase;
+
+public class MapPaneTest extends RepositoryTestCase {
+
+    @Override
+    protected void setUpInternal() throws Exception {
+        insertAndAdd(points1, points2, points3);
+        // commit("points");
+        insertAndAdd(lines1, lines2, lines3);
+        // commit("lines");
+    }
+
+    @Test
+    @Ignore
+    public void show() throws Exception {
+        GeoGIG geogig = getGeogig();
+        Repository repository = geogig.getRepository();
+        MapPane mapPane = new MapPane(repository);
+        mapPane.show();
+        Thread.sleep(2000);
+    }
+}

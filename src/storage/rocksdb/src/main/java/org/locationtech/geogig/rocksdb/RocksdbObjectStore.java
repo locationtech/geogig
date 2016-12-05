44f5056832cbf2e89a7fe6ec22241a74480860e5
Gabriel Roldan <groldan@boundlessgeo.com>
2016-12-05 11:17:34 -0300

    Simplify RocksdbObjectStore.getAll(...)
    Signed-off-by: Gabriel Roldan <groldan@boundlessgeo.com>

--- src/storage/rocksdb/src/main/java/org/locationtech/geogig/rocksdb/RocksdbObjectStore.java
+++ src/storage/rocksdb/src/main/java/org/locationtech/geogig/rocksdb/RocksdbObjectStore.java
@@ -15,15 +15,11 @@ import static com.google.common.base.Preconditions.checkNotNull;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
-import java.util.SortedSet;
-import java.util.TreeSet;
 
 import org.eclipse.jdt.annotation.Nullable;
 import org.locationtech.geogig.model.ObjectId;
@@ -43,13 +39,10 @@ import org.rocksdb.RocksIterator;
 import org.rocksdb.WriteBatch;
 import org.rocksdb.WriteOptions;
 
-import com.google.common.base.Function;
 import com.google.common.base.Optional;
 import com.google.common.base.Preconditions;
-import com.google.common.base.Predicate;
-import com.google.common.base.Predicates;
 import com.google.common.base.Throwables;
-import com.google.common.collect.Iterables;
+import com.google.common.collect.AbstractIterator;
 import com.google.common.collect.Iterators;
 import com.google.inject.Inject;
 
@@ -64,6 +57,8 @@ public class RocksdbObjectStore extends AbstractObjectStore implements ObjectSto
     private DBHandle dbhandle;
 
     private RocksDB db;
+
+    private ReadOptions bulkReadOptions;
 
     @Inject
     public RocksdbObjectStore(Platform platform, @Nullable Hints hints) {
@@ -86,6 +81,9 @@ public class RocksdbObjectStore extends AbstractObjectStore implements ObjectSto
         DBOptions address = new DBOptions(path, readOnly);
         this.dbhandle = RocksConnectionManager.INSTANCE.acquire(address);
         this.db = dbhandle.db;
+        this.bulkReadOptions = new ReadOptions();
+        this.bulkReadOptions.setFillCache(false);
+        this.bulkReadOptions.setVerifyChecksums(false);
         open = true;
     }
 
@@ -99,7 +97,7 @@ public class RocksdbObjectStore extends AbstractObjectStore implements ObjectSto
         final DBHandle dbhandle = this.dbhandle;
         this.db = null;
         this.dbhandle = null;
-
+        this.bulkReadOptions.close();
         RocksConnectionManager.INSTANCE.release(dbhandle);
     }
 
@@ -124,11 +122,8 @@ public class RocksdbObjectStore extends AbstractObjectStore implements ObjectSto
         checkWritable();
         byte[] key = id.getRawValue();
         boolean exists;
-        try (ReadOptions ro = new ReadOptions()) {
-            ro.setFillCache(false);
-            ro.setVerifyChecksums(false);
-            exists = exists(ro, key);
-        }
+        exists = exists(bulkReadOptions, key);
+
         if (!exists) {
             try {
                 db.put(key, rawData);
@@ -167,10 +162,8 @@ public class RocksdbObjectStore extends AbstractObjectStore implements ObjectSto
     public boolean exists(ObjectId id) {
         checkOpen();
         checkNotNull(id, "argument id is null");
-        try (ReadOptions ro = new ReadOptions()) {
-            ro.setFillCache(false).setVerifyChecksums(false);
-            return exists(ro, id.getRawValue());
-        }
+
+        return exists(bulkReadOptions, id.getRawValue());
     }
 
     private static final byte[] NO_DATA = new byte[0];
@@ -205,63 +198,6 @@ public class RocksdbObjectStore extends AbstractObjectStore implements ObjectSto
         return getAll(ids, listener, RevObject.class);
     }
 
-    private static class BatchGet implements Function<List<ObjectId>, Iterator<RevObject>> {
-
-        private final RocksdbObjectStore store;
-
-        private final BulkOpListener listener;
-
-        private final Predicate<Object> filter;
-
-        public BatchGet(RocksdbObjectStore store, BulkOpListener listener, final Class<?> filter) {
-            this.store = store;
-            this.listener = listener;
-            this.filter = RevObject.class.equals(filter) ? Predicates.alwaysTrue()
-                    : Predicates.instanceOf(filter);
-        }
-
-        @Override
-        public Iterator<RevObject> apply(List<ObjectId> input) {
-            store.checkOpen();
-            SortedSet<ObjectId> sortedIds = new TreeSet<>(input);
-            List<RevObject> objects = new ArrayList<>(input.size());
-            try (ReadOptions ops = new ReadOptions()) {
-                ops.setFillCache(false);// better for bulk ops
-                ops.setVerifyChecksums(false);
-
-                byte[] keybuff = new byte[ObjectId.NUM_BYTES];
-
-                try (RocksIterator rocksit = store.db.newIterator(ops)) {
-                    for (ObjectId id : sortedIds) {
-                        RevObject object = null;
-                        id.getRawValue(keybuff);
-                        rocksit.seek(keybuff);
-                        if (rocksit.isValid()) {
-                            byte[] currentKey = rocksit.key();
-                            if (Arrays.equals(keybuff, currentKey)) {
-                                byte[] value = rocksit.value();
-                                try {
-                                    object = store.serializer.read(id,
-                                            new ByteArrayInputStream(value));
-                                    if (filter.apply(object)) {
-                                        objects.add(object);
-                                        listener.found(id, Integer.valueOf(value.length));
-                                        continue;
-                                    }
-                                } catch (IOException e) {
-                                    throw Throwables.propagate(e);
-                                }
-                            }
-                        }
-                        listener.notFound(id);
-                    }
-                }
-            }
-            return objects.iterator();
-        }
-    }
-
-    @SuppressWarnings("unchecked")
     @Override
     public <T extends RevObject> Iterator<T> getAll(final Iterable<ObjectId> ids,
             final BulkOpListener listener, final Class<T> type) {
@@ -270,14 +206,45 @@ public class RocksdbObjectStore extends AbstractObjectStore implements ObjectSto
         checkNotNull(type, "type is null");
         checkOpen();
 
-        final int partitionSize = 500;
-        Iterator<List<ObjectId>> partitions = Iterables.partition(ids, partitionSize).iterator();
+        return new AbstractIterator<T>() {
 
-        Iterator<Iterator<RevObject>> objects = Iterators.transform(partitions,
-                new BatchGet(this, listener, type));
+            private Iterator<ObjectId> oids = ids.iterator();
 
-        Iterator<RevObject> iterator = Iterators.concat(objects);
-        return (Iterator<T>) iterator;
+            private byte[] keybuff = new byte[ObjectId.NUM_BYTES];
+
+            private byte[] valueBuff = new byte[64 * 1024];
+
+            private ReadOptions readOps = bulkReadOptions;
+
+            @Override
+            protected T computeNext() {
+                checkOpen();
+                try {
+                    while (oids.hasNext()) {
+                        ObjectId id = oids.next();
+                        id.getRawValue(keybuff);
+                        final int size = db.get(readOps, keybuff, valueBuff);
+                        if (RocksDB.NOT_FOUND == size) {
+                            listener.notFound(id);
+                            continue;
+                        }
+                        if (size > valueBuff.length) {
+                            valueBuff = db.get(readOps, keybuff);
+                        }
+                        RevObject object = serializer.read(id, new ByteArrayInputStream(valueBuff));
+                        if (type.isInstance(object)) {
+                            listener.found(id, Integer.valueOf(size));
+                            return type.cast(object);
+                        } else {
+                            listener.notFound(id);
+                        }
+                    }
+                } catch (Exception e) {
+                    throw Throwables.propagate(e);
+                }
+                return endOfData();
+            }
+        };
     }
 
     @Override

58dbf12b3f37e2c63268ec986d519a5a387d1298
dblasby <dblasby@boundlessgeo.com>
2017-01-04 17:23:44 -0800

    add comment to QuadTreeClusteringStrategy

--- src/core/src/main/java/org/locationtech/geogig/model/internal/QuadTreeClusteringStrategy.java
+++ src/core/src/main/java/org/locationtech/geogig/model/internal/QuadTreeClusteringStrategy.java
@@ -19,6 +19,44 @@ import org.locationtech.geogig.model.RevTree;
 import com.google.common.base.Optional;
 import com.vividsolutions.jts.geom.Envelope;
 
+
+/**
+ *  This class determines how the quadtree clustering strategy puts features in the tree.
+ *
+ *  NOTE: This also supports polygons/lines (i.e. non-point objects).
+ *        Each feature is only put in ONE location in the tree - the quad that it
+ *        is fully enclosed by (which might be the root node).
+ *
+ *  NOTE: if the feature doesn't have a bounds (i.e. null or empty), then it is
+ *        NOT put in the tree (null QuadTreeNodeId).
+ *
+ *  The main entry point is computeId(), which returns a QuadTreeNodeId that defines
+ *  where that Node should be in the QuadTree.
+ *  The QuadTreeNodeId contains a list of Quandrants defining which quadrant the feature
+ *  lies completely inside for EACH level of the quadtree.
+ *
+ *  If QuadTreeNodeId contains quadrants NE, then SE, then NW then it means that:
+ *
+ *  The feature is completely in the NE quadrant for the "world" (level 0).
+ *
+ *  Looking at the world's NE quadrant and subdividing it into 4 quads,
+ *   the feature would be fully in the SE quadrant (level 1).
+ *
+ *  The world's NE quadrant (level 0), then that quad's sub SE quadrant (level 1),
+ *  the feature will be contains in that quad's NW quad (level 2).
+ *
+ *  This continues until;
+ *     + the max depth of the tree is reached
+ *     + the feature in not fully contained in a single quad
+ *
+ *  Typically, large features will need to be contained "higher" up in the tree.
+ *  However, this can happen with very small features that are "unlucky" enough to cross
+ *  ANY quad boundary.  If this happens a lot, then the tree could become more degenerative
+ *  and have large numbers of features associated with a single node that can NOT be moved
+ *  into "lower" level nodes in the hierarchy.
+ *
+ *
+ */
 class QuadTreeClusteringStrategy extends ClusteringStrategy {
 
     private final Envelope maxBounds;
@@ -32,11 +70,7 @@ class QuadTreeClusteringStrategy extends ClusteringStrategy {
         this.maxDepth = maxDepth;
     }
 
-    // @Override
-    // protected int maxDepth() {
-    // return 12;
-    // }
-    //
+
     @Override
     protected int maxBuckets(final int depthIndex) {
         return 4;

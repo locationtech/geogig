3afb0feaa956aafa78fd6dc198b6961418a7dbb7
Johnathan Garrett <jd@prominentedge.com>
2017-03-29 00:06:51 -0400

    Add unit tests for concurrently building indexes.
    Signed-off-by: Johnathan Garrett <jd@prominentedge.com>

--- src/core/src/test/java/org/locationtech/geogig/plumbing/index/BuildIndexOpTest.java
+++ src/core/src/test/java/org/locationtech/geogig/plumbing/index/BuildIndexOpTest.java
@@ -3,12 +3,17 @@ package org.locationtech.geogig.plumbing.index;
 import static org.locationtech.geogig.model.impl.RevObjectTestSupport.getTreeNodes;
 import static org.locationtech.geogig.plumbing.index.QuadTreeTestSupport.createPointFeature;
 import static org.locationtech.geogig.plumbing.index.QuadTreeTestSupport.createWorldPointsLayer;
+import static org.locationtech.geogig.plumbing.index.QuadTreeTestSupport.getPointFid;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 import org.eclipse.jdt.annotation.Nullable;
 import org.geotools.feature.simple.SimpleFeatureBuilder;
@@ -20,6 +25,10 @@ import org.locationtech.geogig.model.RevFeature;
 import org.locationtech.geogig.model.RevTree;
 import org.locationtech.geogig.model.impl.CanonicalTreeBuilder;
 import org.locationtech.geogig.model.impl.RevFeatureBuilder;
+import org.locationtech.geogig.porcelain.BranchCreateOp;
+import org.locationtech.geogig.porcelain.CheckoutOp;
+import org.locationtech.geogig.porcelain.RemoveOp;
+import org.locationtech.geogig.porcelain.index.IndexUtils;
 import org.locationtech.geogig.repository.IndexInfo;
 import org.locationtech.geogig.repository.IndexInfo.IndexType;
 import org.locationtech.geogig.repository.Repository;
@@ -176,6 +185,121 @@ public class BuildIndexOpTest extends RepositoryTestCase {
         IndexInfo indexInfo = createIndexForLayer(pointsName, extraAttributes);
         RevTree indexTree = updateIndex(indexInfo, featureTypeId, RevTree.EMPTY, canonicalTree);
         assertEquals(canonicalTree.size(), indexTree.size());
+    }
+
+    @Test
+    public void testConcurrentlyBuildingSameIndex() throws Exception {
+        indexInfo = createIndex();
+        final int threadCount = 4;
+
+        ExecutorService executor = Executors.newFixedThreadPool(threadCount);
+        List<Future<?>> futures = new ArrayList<>();
+        final RevTree oldCanonicalTree = RevTree.EMPTY;
+        final RevTree newCanonicalTree = worldPointsTree;
+        for (int t = 0; t < threadCount; t++) {
+            Future<?> future = executor.submit(new Runnable() {
+                @Override
+                public void run() {
+                    RevTree indexTree = updateIndex(oldCanonicalTree, newCanonicalTree);
+
+                    assertNotEquals(RevTree.EMPTY, indexTree);
+                    assertEquals(newCanonicalTree.size(), indexTree.size());
+
+                    IndexTestSupport.verifyIndex(geogig, indexTree.getId(),
+                            newCanonicalTree.getId());
+                }
+            });
+            futures.add(future);
+        }
+
+        for (Future<?> f : futures) {
+            f.get();
+        }
+    }
+
+    @Test
+    public void testConcurrentlyBuildingDifferentIndexes() throws Exception {
+        Repository repository = getRepository();
+        // Create several branches
+        repository.command(BranchCreateOp.class).setName("branch1").call();
+        repository.command(BranchCreateOp.class).setName("branch2").call();
+        repository.command(BranchCreateOp.class).setName("branch3").call();
+        repository.command(BranchCreateOp.class).setName("branch4").call();
+
+        List<RevTree> branchTrees = new ArrayList<RevTree>(4);
+        // Make different changes on each branch
+        repository.command(CheckoutOp.class).setSource("branch1").call();
+        String fid1 = getPointFid(0, 0);
+        repository.command(RemoveOp.class)
+                .addPathToRemove(NodeRef.appendChild(worldPointsLayer.getName(), fid1)).call();
+        super.add();
+        super.commit("deleted 0, 0");
+        NodeRef featureTree = IndexUtils.resolveTypeTreeRef(geogig.getContext(),
+                "branch1:" + worldPointsLayer.getName());
+        branchTrees.add(repository.objectDatabase().getTree(featureTree.getObjectId()));
+        repository.command(CheckoutOp.class).setSource("branch2").call();
+        String fid2 = getPointFid(0, 5);
+        repository.command(RemoveOp.class)
+                .addPathToRemove(NodeRef.appendChild(worldPointsLayer.getName(), fid2)).call();
+        super.add();
+        super.commit("deleted 0, 5");
+        featureTree = IndexUtils.resolveTypeTreeRef(geogig.getContext(),
+                "branch2:" + worldPointsLayer.getName());
+        branchTrees.add(repository.objectDatabase().getTree(featureTree.getObjectId()));
+        repository.command(CheckoutOp.class).setSource("branch3").call();
+        String fid3 = getPointFid(0, 10);
+        repository.command(RemoveOp.class)
+                .addPathToRemove(NodeRef.appendChild(worldPointsLayer.getName(), fid3)).call();
+        super.add();
+        super.commit("deleted 0, 10");
+        featureTree = IndexUtils.resolveTypeTreeRef(geogig.getContext(),
+                "branch3:" + worldPointsLayer.getName());
+        branchTrees.add(repository.objectDatabase().getTree(featureTree.getObjectId()));
+        repository.command(CheckoutOp.class).setSource("branch4").call();
+        String fid4 = getPointFid(0, 15);
+        repository.command(RemoveOp.class)
+                .addPathToRemove(NodeRef.appendChild(worldPointsLayer.getName(), fid4)).call();
+        super.add();
+        super.commit("deleted 0, 15");
+        featureTree = IndexUtils.resolveTypeTreeRef(geogig.getContext(),
+                "branch4:" + worldPointsLayer.getName());
+        branchTrees.add(repository.objectDatabase().getTree(featureTree.getObjectId()));
+        repository.command(CheckoutOp.class).setSource("master").call();
+
+        indexInfo = createIndex();
+
+        // Build root index tree
+        final RevTree oldCanonicalTree = RevTree.EMPTY;
+        final RevTree newCanonicalTree = worldPointsTree;
+
+        RevTree indexTree = updateIndex(oldCanonicalTree, newCanonicalTree);
+
+        assertNotEquals(RevTree.EMPTY, indexTree);
+        assertEquals(newCanonicalTree.size(), indexTree.size());
+
+        IndexTestSupport.verifyIndex(geogig, indexTree.getId(), newCanonicalTree.getId());
+
+        ExecutorService executor = Executors.newFixedThreadPool(branchTrees.size());
+        List<Future<?>> futures = new ArrayList<>();
+        for (final RevTree branchCanonicalTree : branchTrees) {
+            Future<?> future = executor.submit(new Runnable() {
+                @Override
+                public void run() {
+                    RevTree indexTree = updateIndex(newCanonicalTree, branchCanonicalTree);
+
+                    assertNotEquals(RevTree.EMPTY, indexTree);
+                    assertEquals(branchCanonicalTree.size(), indexTree.size());
+
+                    IndexTestSupport.verifyIndex(geogig, indexTree.getId(),
+                            branchCanonicalTree.getId());
+                }
+            });
+            futures.add(future);
+        }
+
+        for (Future<?> f : futures) {
+            f.get();
+        }
     }
 
     private RevTree checkUpdatesNodes() {

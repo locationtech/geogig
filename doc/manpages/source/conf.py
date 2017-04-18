e11d0932160b35756a8bfa72dc3160db7289c7ea
Gabriel Roldan <groldan@boundlessgeo.com>
2017-04-18 11:01:19 -0300

    Set version to 1.2-SNAPSHOT
    Signed-off-by: Gabriel Roldan <groldan@boundlessgeo.com>

--- doc/manpages/source/conf.py
+++ doc/manpages/source/conf.py
@@ -97,12 +97,12 @@ copyright = u'Boundless <http://boundlessgeo.com>'
 # other places throughout the built documents.
 #
 # The short X.Y version.
-version = '1.1'
+version = '1.2'
 # The full version, including alpha/beta/rc tags.
-release = '1.1-SNAPSHOT'
+release = '1.2-SNAPSHOT'
 # Users don't need to see the "SNAPSHOT" notation when it's there
 if release.find('SNAPSHOT') != -1:
-   release = '1.1'
+   release = '1.2'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:

fa1e6bf6b7eea66a0e28abc15daa1063af8c95ab
Gabriel Roldan <groldan@boundlessgeo.com>
2017-06-02 11:09:39 -0300

    Fix empty string bug in custom geotools converter factories
    Fix a bug in PrimitiveArrayToStringConverterFactory that
    returned an empty array when converting an empty string to
    a primitive array instead of null.
    
    It had the side effect of breaking the geoserver config UI
    for ImageMosaic layers as it uses the Converters framework
    to parse the "Bands" argument to an int[] that comes as an
    empty string.
    
    This patch makes the same fix to our MapToStringConverterFactory
    although no bug report was issued for it, for the sake of
    consistency.
    
    Signed-off-by: Gabriel Roldan <groldan@boundlessgeo.com>

--- src/core/src/test/java/org/locationtech/geogig/data/MapToStringConverterFactoryTest.java
+++ src/core/src/test/java/org/locationtech/geogig/data/MapToStringConverterFactoryTest.java
@@ -1,4 +1,4 @@
-/* Copyright (c) 2015-2016 Boundless and others.
+/* Copyright (c) 2015-2017 Boundless and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Distribution License v1.0
  * which accompanies this distribution, and is available at
@@ -12,24 +12,41 @@ package org.locationtech.geogig.data;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Objects;
 
 import org.geotools.util.Converters;
 import org.junit.Test;
 
 import com.google.common.collect.ImmutableMap;
 
+/**
+ * Test suite for {@link MapToStringConverterFactory}
+ *
+ */
 public class MapToStringConverterFactoryTest {
+    
+    @Test
+    public void nullTest(){
+        assertNull(Converters.convert(null, Map.class));
+        assertNull(Converters.convert("", Map.class));
+        assertNull(Converters.convert(" ", Map.class));
+    }
 
     @Test
     public void roundtripTest() {
 
-        Map<String, ? extends Object> map = ImmutableMap.of(//
-                "key1", "value1", //
-                "key:2", "value|2", //
-                "key3", Integer.valueOf(12345), //
-                "submap", ImmutableMap.of("submap1", "subvalue1", "submap2", new Long(789)));
+        Map<String, Object> map = new HashMap<>();
+        map.put("key1", "value1");
+        map.put("key:2", "value|2");
+        map.put("key3", Integer.valueOf(12345));
+        map.put("nullval", null);
+        map.put("arrayval", new double[] { Math.PI, Double.MIN_VALUE, Double.MAX_VALUE });
+        map.put("submap", ImmutableMap.of("submap1", "subvalue1", "submap2", new Long(789)));
 
         assertNull(Converters.convert(map, Integer.class));
 
@@ -40,7 +57,12 @@ public class MapToStringConverterFactoryTest {
         Map<String, Object> roundTripped = Converters.convert(converted, Map.class);
         assertNotNull(roundTripped);
 
-        assertEquals(map, roundTripped);
+        assertEquals(map.size(), roundTripped.size());
+        assertEquals(map.keySet(), roundTripped.keySet());
+
+        for (Entry<String, Object> e : map.entrySet()) {
+            assertTrue(Objects.deepEquals(e.getValue(), roundTripped.get(e.getKey())));
+        }
     }
 
 }

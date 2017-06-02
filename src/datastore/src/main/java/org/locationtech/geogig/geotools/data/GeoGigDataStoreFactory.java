6c9000328a32ca549937d9b19d5b13438732158c
Gabriel Roldan <groldan@boundlessgeo.com>
2017-06-02 12:16:40 -0300

    Make clear AUTO_INDEXING is a GeoServer related DataStore parameter
    Signed-off-by: Gabriel Roldan <groldan@boundlessgeo.com>

--- src/datastore/src/main/java/org/locationtech/geogig/geotools/data/GeoGigDataStoreFactory.java
+++ src/datastore/src/main/java/org/locationtech/geogig/geotools/data/GeoGigDataStoreFactory.java
@@ -19,6 +19,7 @@ import java.util.Map;
 
 import org.eclipse.jdt.annotation.Nullable;
 import org.geotools.data.DataStoreFactorySpi;
+import org.geotools.data.Parameter;
 import org.locationtech.geogig.repository.Context;
 import org.locationtech.geogig.repository.Hints;
 import org.locationtech.geogig.repository.Repository;
@@ -31,6 +32,7 @@ import org.slf4j.LoggerFactory;
 
 import com.google.common.base.Preconditions;
 import com.google.common.base.Throwables;
+import com.google.common.collect.ImmutableMap;
 
 public class GeoGigDataStoreFactory implements DataStoreFactorySpi {
 
@@ -80,7 +82,10 @@ public class GeoGigDataStoreFactory implements DataStoreFactorySpi {
             "Optional namespace for feature types that do not declare a Namespace themselves",
             false);
 
-    public static final Param AUTO_INDEXING = new Param("autoIndexing", Boolean.class, "Auto-indexing", false);
+    public static final Param AUTO_INDEXING = new Param("autoIndexing", Boolean.class,
+            "Let GeoServer automatically create spatial indexes and add time/elevation attributes if the layer has them",
+            false/* required */, false/* default */, //
+            ImmutableMap.of(Parameter.LEVEL, "advanced"));
 
     @Override
     public String getDisplayName() {
@@ -96,7 +101,7 @@ public class GeoGigDataStoreFactory implements DataStoreFactorySpi {
     public Param[] getParametersInfo() {
         return new Param[] { REPOSITORY, BRANCH, HEAD, DEFAULT_NAMESPACE, AUTO_INDEXING };
     }
-    
+
     private URI resolveURI(String repoParam) {
         URI repoUri = null;
         try {

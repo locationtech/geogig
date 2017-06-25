5ed4c0d6916453ce74e5c7f29d72f9f38998eb7e
Gabriel Roldan <groldan@boundlessgeo.com>
2017-06-25 12:41:23 -0300

    Split rocks object store test into pure store conformance test and object database specific tests
    Signed-off-by: Gabriel Roldan <groldan@boundlessgeo.com>

---
+++ src/storage/rocksdb/src/test/java/org/locationtech/geogig/rocksdb/RocksdbObjectDatabaseTest.java
@@ -0,0 +1,119 @@
+/* Copyright (c) 2017 Boundless and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Distribution License v1.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/org/documents/edl-v10.html
+ *
+ * Contributors:
+ * Gabriel Roldan (Boundless) - initial implementation
+ */
+package org.locationtech.geogig.rocksdb;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import org.locationtech.geogig.repository.Hints;
+import org.locationtech.geogig.storage.BlobStore;
+import org.locationtech.geogig.storage.ConflictsDatabase;
+import org.locationtech.geogig.storage.ObjectStore;
+import org.locationtech.geogig.storage.StorageType;
+import org.locationtech.geogig.storage.datastream.SerializationFactoryProxy;
+import org.locationtech.geogig.storage.fs.IniFileConfigDatabase;
+import org.locationtech.geogig.test.TestPlatform;
+
+/**
+ * Test suite for {@link RocksdbObjectDatabase} methods that are not par of {@link ObjectStore}
+ * (i.e. complements {@link RocksdbObjectStoreConformanceTest})
+ *
+ */
+public class RocksdbObjectDatabaseTest {
+
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
+
+    private TestPlatform platform;
+
+    private IniFileConfigDatabase configDB;
+
+    private Hints hints;
+
+    protected RocksdbObjectDatabase db;
+
+    @Before
+    public void setUp() throws IOException {
+        File root = folder.getRoot();
+        folder.newFolder(".geogig");
+        File home = folder.newFolder("home");
+        platform = new TestPlatform(root);
+        platform.setUserHome(home);
+        hints = new Hints();
+        configDB = new IniFileConfigDatabase(platform);
+        db = new RocksdbObjectDatabase(platform, hints, configDB);
+        db.open();
+    }
+
+    @After
+    public void closeDbs() throws IOException {
+        if (db != null) {
+            db.close();
+        }
+        if (configDB != null) {
+            configDB.close();
+        }
+    }
+
+    @Test
+    public void testOpenTwice() {
+        assertTrue(db.isOpen());
+        // database already opened, try calling open again.
+        db.open();
+        assertTrue(db.isOpen());
+    }
+
+    @Test
+    public void testNoHints() {
+        db.close();
+        db = new RocksdbObjectDatabase(platform, null, configDB);
+        db.open();
+        db.checkWritable();
+    }
+
+    @Test
+    public void testAccessors() {
+        assertFalse(db.isReadOnly());
+        ConflictsDatabase conflicts = db.getConflictsDatabase();
+        assertTrue(conflicts instanceof RocksdbConflictsDatabase);
+        BlobStore blobStore = db.getBlobStore();
+        assertTrue(blobStore instanceof RocksdbBlobStore);
+    }
+
+    @Test
+    public void testConfigure() throws Exception {
+        db.configure();
+        assertEquals(RocksdbStorageProvider.VERSION,
+                configDB.get(RocksdbStorageProvider.FORMAT_NAME + ".version").get());
+        assertEquals(RocksdbStorageProvider.FORMAT_NAME,
+                configDB.get("storage." + StorageType.OBJECT.key).get());
+
+        assertTrue(db.checkConfig());
+
+    }
+
+    @Test
+    public void testSerializer() {
+        assertTrue(db.serializer() instanceof SerializationFactoryProxy);
+        db.close();
+        db.open();
+        assertTrue(db.serializer() instanceof SerializationFactoryProxy);
+    }
+
+}

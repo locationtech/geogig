b7b4a0270fba958bfe63875f329e0a4b06831fed
Gabriel Roldan <groldan@boundlessgeo.com>
2017-06-25 12:41:23 -0300

    Split rocks object store test into pure store conformance test and object database specific tests
    Signed-off-by: Gabriel Roldan <groldan@boundlessgeo.com>

--- src/storage/rocksdb/src/test/java/org/locationtech/geogig/rocksdb/RocksdbObjectStoreConformanceTest.java
+++ src/storage/rocksdb/src/test/java/org/locationtech/geogig/rocksdb/RocksdbObjectStoreConformanceTest.java
@@ -9,80 +9,16 @@
  */
 package org.locationtech.geogig.rocksdb;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
-
-import org.junit.Test;
 import org.locationtech.geogig.repository.Hints;
 import org.locationtech.geogig.repository.Platform;
-import org.locationtech.geogig.storage.BlobStore;
-import org.locationtech.geogig.storage.ConfigDatabase;
-import org.locationtech.geogig.storage.ConflictsDatabase;
-import org.locationtech.geogig.storage.StorageType;
-import org.locationtech.geogig.storage.datastream.SerializationFactoryProxy;
-import org.locationtech.geogig.storage.fs.IniFileConfigDatabase;
 import org.locationtech.geogig.storage.impl.ObjectStoreConformanceTest;
 
 public class RocksdbObjectStoreConformanceTest extends ObjectStoreConformanceTest {
 
-    private Platform platform = null;
-
-    private ConfigDatabase configDB = null;
-
-    private RocksdbObjectDatabase database = null;
-
     @Override
-    protected RocksdbObjectDatabase createOpen(Platform platform, Hints hints) {
-        this.platform = platform;
-        configDB = new IniFileConfigDatabase(platform);
-        database = new RocksdbObjectDatabase(platform, hints, configDB);
+    protected RocksdbObjectStore createOpen(Platform platform, Hints hints) {
+        RocksdbObjectStore database = new RocksdbObjectStore(platform, hints);
         database.open();
         return database;
     }
-
-    @Test
-    public void testOpenTwice() {
-        assertTrue(db.isOpen());
-        // database already opened, try calling open again.
-        db.open();
-        assertTrue(db.isOpen());
-    }
-
-    @Test
-    public void testNoHints() {
-        db = closeAndCreate(db, platform, null);
-        ((RocksdbObjectStore) db).checkWritable();
-    }
-
-    @Test
-    public void testAccessors() {
-        assertFalse(database.isReadOnly());
-        ConflictsDatabase conflicts = database.getConflictsDatabase();
-        assertNotNull(conflicts);
-        BlobStore blobStore = database.getBlobStore();
-        assertNotNull(blobStore);
-    }
-
-    @Test
-    public void testConfigure() throws Exception {
-        database.configure();
-        assertEquals(RocksdbStorageProvider.VERSION,
-                configDB.get(RocksdbStorageProvider.FORMAT_NAME + ".version").get());
-        assertEquals(RocksdbStorageProvider.FORMAT_NAME,
-                configDB.get("storage." + StorageType.OBJECT.key).get());
-
-        assertTrue(database.checkConfig());
-
-    }
-
-    @Test
-    public void testSerializer() {
-        assertTrue(database.serializer() instanceof SerializationFactoryProxy);
-        database.close();
-        database.open();
-        assertTrue(database.serializer() instanceof SerializationFactoryProxy);
-    }
-
 }

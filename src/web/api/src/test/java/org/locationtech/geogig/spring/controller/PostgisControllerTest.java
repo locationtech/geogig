20c8c02b5c75b62b1c9201adefd1e717e1bd69c6
Johnathan Garrett <jd@prominentedge.com>
2017-08-28 17:59:07 -0400

    Added postgis controller and tests.

---
+++ src/web/api/src/test/java/org/locationtech/geogig/spring/controller/PostgisControllerTest.java
@@ -0,0 +1,97 @@
+/* Copyright (c) 2017 Boundless and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Distribution License v1.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/org/documents/edl-v10.html
+ *
+ * Contributors:
+ * Johnathan Garrett (Prominent Edge) - initial implementation
+ */
+package org.locationtech.geogig.spring.controller;
+
+import static org.hamcrest.core.IsNot.not;
+import static org.junit.Assert.assertEquals;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.io.StringReader;
+import java.util.List;
+
+import javax.json.Json;
+import javax.json.JsonObject;
+
+import org.junit.Test;
+import org.locationtech.geogig.geotools.cli.TestHelper;
+import org.locationtech.geogig.model.NodeRef;
+import org.locationtech.geogig.model.Ref;
+import org.locationtech.geogig.plumbing.LsTreeOp;
+import org.locationtech.geogig.plumbing.LsTreeOp.Strategy;
+import org.locationtech.geogig.plumbing.RefParse;
+import org.locationtech.geogig.repository.Repository;
+import org.locationtech.geogig.rest.AsyncContext;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import com.google.common.collect.Lists;
+
+public class PostgisControllerTest extends AbstractControllerTest {
+
+    @Test
+    public void testImport() throws Exception {
+        PostgisController.dataStoreFactory = TestHelper.createTestFactory();
+        repoProvider.createGeogig("testRepo", null);
+        repoProvider.getTestRepository("testRepo").initializeRpository();
+
+        MockHttpServletRequestBuilder importRequest = MockMvcRequestBuilders
+                .get("/repos/testRepo/postgis/import.json");
+        importRequest.param("table", "table1");
+
+        MvcResult result = perform(importRequest).andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.task").exists()).andExpect(jsonPath("$.task.id").exists())
+                .andExpect(jsonPath("$.task.status").value("RUNNING")).andReturn();
+
+        String jsonString = result.getResponse().getContentAsString();
+        JsonObject rootObject = Json.createReader(new StringReader(jsonString)).readObject();
+        JsonObject taskObject = rootObject.getJsonObject("task");
+        int taskId = taskObject.getInt("id");
+
+        result = waitForTask(taskId)
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.task").exists())
+                .andExpect(jsonPath("$.task.id").value(taskId))
+                .andExpect(jsonPath("$.task.status").value("FINISHED"))
+                .andExpect(jsonPath("$.task.result.RevTree.treeId").exists()).andReturn();
+
+        jsonString = result.getResponse().getContentAsString();
+        rootObject = Json.createReader(new StringReader(jsonString)).readObject();
+        String treeIdStr = rootObject.getJsonObject("task").getJsonObject("result")
+                .getJsonObject("RevTree").getString("treeId");
+        Repository geogig = repoProvider.getGeogig("testRepo").get();
+        Ref workHead = geogig.command(RefParse.class).setName(Ref.WORK_HEAD).call().get();
+        assertEquals(treeIdStr, workHead.getObjectId().toString());
+
+        List<NodeRef> nodes = Lists.newArrayList(geogig.command(LsTreeOp.class)
+                .setReference(Ref.WORK_HEAD).setStrategy(Strategy.DEPTHFIRST_ONLY_FEATURES).call());
+        assertEquals(2, nodes.size());
+
+    }
+
+    private ResultActions waitForTask(Integer taskId) throws Exception {
+        while (true) {
+            Thread.sleep(100);
+            MockHttpServletRequestBuilder taskRequest = MockMvcRequestBuilders
+                    .get(String.format("/tasks/%d.json", taskId));
+            try {
+                return perform(taskRequest).andExpect(jsonPath("$.task.status")
+                        .value(not(AsyncContext.Status.RUNNING.toString())));
+            } catch (AssertionError e) {
+                // ignore
+            }
+        }
+    }
+}
--- src/web/api/src/test/java/org/locationtech/geogig/spring/controller/RepositoryInitControllerTest.java
+++ src/web/api/src/test/java/org/locationtech/geogig/spring/controller/RepositoryInitControllerTest.java
@@ -13,41 +13,12 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.locationtech.geogig.rest.repository.RepositoryProvider;
-import org.locationtech.geogig.rest.repository.TestMultiRepositoryProvider;
-import org.locationtech.geogig.spring.config.GeoGigWebAPISpringConfig;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = { GeoGigWebAPISpringConfig.class })
-@WebAppConfiguration
-public class RepositoryInitControllerTest {
-
-    @Rule
-    public TestMultiRepositoryProvider repoProvider = new TestMultiRepositoryProvider();
-
-    @Autowired
-    private WebApplicationContext appContext;
-
-    private MockMvc mockMvc;
-
-    @Before
-    public void setup() {
-        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.appContext).build();
-    }
+public class RepositoryInitControllerTest extends AbstractControllerTest {
 
     @Test
     public void testFailWithInitializedRepository() throws Exception {
@@ -56,10 +27,8 @@ public class RepositoryInitControllerTest {
 
         MockHttpServletRequestBuilder initRequest = MockMvcRequestBuilders
                 .put("/repos/testRepo/init.json");
-        // need to embed a RepsoitoryResolver into the request
-        initRequest.requestAttr(RepositoryProvider.KEY, repoProvider);
 
-        mockMvc.perform(initRequest).andExpect(status().isConflict())
+        perform(initRequest).andExpect(status().isConflict())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("$.response.success").value(false))
                 .andExpect(jsonPath("$.response.error")
@@ -70,10 +39,8 @@ public class RepositoryInitControllerTest {
     public void testInitRepo() throws Exception {
         MockHttpServletRequestBuilder initRequest = MockMvcRequestBuilders
                 .put("/repos/testRepo/init.json");
-        // need to embed a RepsoitoryResolver into the request
-        initRequest.requestAttr(RepositoryProvider.KEY, repoProvider);
 
-        mockMvc.perform(initRequest).andExpect(status().isCreated())
+        perform(initRequest).andExpect(status().isCreated())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("$.response.success").value(true))
                 .andExpect(jsonPath("$.response.repo.name").value("testRepo"))

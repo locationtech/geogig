2c1256cfb989b5c6a2cb1ccefd4fcb70addd05b8
Gabriel Roldan <groldan@boundlessgeo.com>
2017-09-28 12:14:21 -0300

    Update (c) headers on geogig-remoting
    Signed-off-by: Gabriel Roldan <groldan@boundlessgeo.com>

--- src/remoting/src/main/java/org/locationtech/geogig/remotes/LsRemoteOp.java
+++ src/remoting/src/main/java/org/locationtech/geogig/remotes/LsRemoteOp.java
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012-2016 Boundless and others.
+/* Copyright (c) 2012-2017 Boundless and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Distribution License v1.0
  * which accompanies this distribution, and is available at

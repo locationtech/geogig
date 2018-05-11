bf6be914d278d82fd860c317feeca7f24db215c5
Gabriel Roldan <groldan@boundlessgeo.com>
2018-05-11 08:48:12 -0300

    Split MergeScenarioReport unconflicted count into trees and features
    Signed-off-by: Gabriel Roldan <groldan@boundlessgeo.com>

--- src/core/src/main/java/org/locationtech/geogig/plumbing/merge/MergeScenarioReport.java
+++ src/core/src/main/java/org/locationtech/geogig/plumbing/merge/MergeScenarioReport.java
@@ -11,32 +11,34 @@ package org.locationtech.geogig.plumbing.merge;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.locationtech.geogig.model.DiffEntry;
+import org.locationtech.geogig.model.RevObject.TYPE;
+
 /**
  * A class that counts the changes introduced by a branch to be merged, divided in categories
  * according to how they can (or can't) be merged into the destination branch
  */
 public class MergeScenarioReport {
 
-    private final AtomicLong conflicts, unconflicted, merged;
-
-    public MergeScenarioReport() {
-        conflicts = new AtomicLong();
-        unconflicted = new AtomicLong();
-        merged = new AtomicLong();
-    }
+    private final AtomicLong conflicts = new AtomicLong(), unconflictedFeatures = new AtomicLong(),
+            unconflictedTrees = new AtomicLong(), merged = new AtomicLong();
 
     public void addConflict() {
         conflicts.incrementAndGet();
 
     }
 
-    public void addUnconflicted() {
-        unconflicted.incrementAndGet();
+    public void addUnconflicted(DiffEntry diff) {
+        TYPE type = (diff.isAdd() ? diff.getNewObject() : diff.getOldObject()).getType();
+        if (TYPE.FEATURE == type) {
+            unconflictedFeatures.incrementAndGet();
+        } else {
+            unconflictedTrees.incrementAndGet();
+        }
     }
 
     public void addMerged() {
         merged.incrementAndGet();
-
     }
 
     /**
@@ -49,12 +51,24 @@ public class MergeScenarioReport {
     }
 
     /**
-     * Returns the number of unconflicted features.
-     * 
-     * @return
+     * @return the number of unconflicted features and trees.
      */
     public long getUnconflicted() {
-        return unconflicted.get();
+        return unconflictedFeatures.get() + unconflictedTrees.get();
+    }
+
+    /**
+     * @return the number of unconflicted features.
+     */
+    public long getUnconflictedFeatures() {
+        return unconflictedFeatures.get();
+    }
+
+    /**
+     * @return the number of unconflicted trees.
+     */
+    public long getUnconflictedTrees() {
+        return unconflictedTrees.get();
     }
 
     /**

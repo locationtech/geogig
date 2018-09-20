6f5335839c2c1f57b349e656edb0bbbc4ee24b5e
Gabriel Roldan <groldan@boundlessgeo.com>
2018-09-20 15:36:05 -0300

    Revert change to IndexInfo that broke equals()
    REVISIT: the actual issue is IndexInfo computes its SHA1
    id on its constructor, but it doesn't take the metadata map
    into account. If it did, equals and hashCode could just
    use the id.
    
    Signed-off-by: Gabriel Roldan <groldan@boundlessgeo.com>

--- src/api/src/main/java/org/locationtech/geogig/repository/IndexInfo.java
+++ src/api/src/main/java/org/locationtech/geogig/repository/IndexInfo.java
@@ -17,7 +17,6 @@ import org.eclipse.jdt.annotation.Nullable;
 import org.locationtech.geogig.model.Node;
 import org.locationtech.geogig.model.ObjectId;
 import org.locationtech.geogig.model.RevTree;
-import org.locationtech.jts.geom.Envelope;
 
 import com.google.common.base.Charsets;
 import com.google.common.base.Preconditions;
@@ -25,12 +24,8 @@ import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Sets;
 import com.google.common.hash.Hasher;
+import org.locationtech.jts.geom.Envelope;
 
-import lombok.EqualsAndHashCode;
-import lombok.ToString;
-
-@EqualsAndHashCode
-@ToString
 public final class IndexInfo {
     public static enum IndexType {
         QUADTREE
@@ -87,9 +82,21 @@ public final class IndexInfo {
         return metadata;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (o instanceof IndexInfo) {
+            IndexInfo i = (IndexInfo) o;
+            return Objects.equals(getTreeName(), i.getTreeName())
+                    && Objects.equals(getAttributeName(), i.getAttributeName())
+                    && Objects.equals(getIndexType(), i.getIndexType())
+                    && compareMetadatas(getMetadata(), i.getMetadata());
+        }
+        return false;
+    }
+
     /**
-     * Properly compares two IndexInfo metadatas. Since some of the values in the map can be
-     * String[], normal Objects.equals() do not work. We use a better comparison.
+     * Properly compares two IndexInfo metadatas.  Since some of the values in the map
+     * can be String[], normal Objects.equals() do not work.  We use a better comparison.
      *
      * @param meta1
      * @param meta2
@@ -101,9 +108,9 @@ public final class IndexInfo {
         if ((meta1 == null) || (meta2 == null))
             return false; // one null, the other not
         if (meta1 == meta2)
-            return true; // both the same reference
+            return true; //both the same reference
         if (meta1.size() != meta2.size())
-            return false; // different # of entries
+            return false; //different # of entries
 
         for (Map.Entry<String, Object> entry : meta1.entrySet()) {
             if (!meta2.containsKey(entry.getKey()))
@@ -114,6 +121,12 @@ public final class IndexInfo {
                 return false;
         }
         return true;
+    }
+
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getTreeName(), getAttributeName(), getIndexType(), getMetadata().size());
     }
 
     public static ObjectId getIndexId(String treeName, String attributeName) {

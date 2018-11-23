1c4a562218b6254da682d40ee17163dd259c98b6
Gabriel Roldan <groldan@boundlessgeo.com>
2018-11-23 14:21:22 -0300

    Add postgres backend commands to create DDL and upgrade database
    CreateDDL creates the DDL script that's used to initialize a
    PostgreSQL geogig database.
    
    PGDatabaseUpgrade upgrades a geogig database from geogig < 1.2.1,
    by adding the dstindex column to the geogig_graph_edge table and
    running rebuild-graph on all its repositories. This is necessary
    to ensure the proper order of parent commit ids when traversing
    the commit graph. Otherwise merge commits might be reported to have
    the left and right source commits swapped.
    
    Signed-off-by: Gabriel Roldan <groldan@boundlessgeo.com>

--- src/storage/postgres/src/main/java/org/locationtech/geogig/storage/postgresql/config/TableNames.java
+++ src/storage/postgres/src/main/java/org/locationtech/geogig/storage/postgresql/config/TableNames.java
@@ -14,145 +14,148 @@ import java.util.List;
 import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableList;
 
-public class TableNames  implements Cloneable{
+public class TableNames implements Cloneable {
 
-    public static final String DEFAULT_SCHEMA = "public";
+	public static final String DEFAULT_SCHEMA = "public";
 
-    public static final String DEFAULT_TABLE_PREFIX = "geogig_";
+	public static final String DEFAULT_TABLE_PREFIX = "geogig_";
 
-    private final String schema;
+	private final String schema;
 
-    private final String prefix;
+	private final String prefix;
 
-    TableNames() {
-        this(DEFAULT_SCHEMA, DEFAULT_TABLE_PREFIX);
-    }
+	public TableNames() {
+		this(DEFAULT_SCHEMA);
+	}
 
-    public TableNames(String schema, String prefix) {
-        Preconditions.checkNotNull(schema);
-        Preconditions.checkNotNull(prefix);
-        this.schema = schema;
-        this.prefix = prefix;
-    }
+	public TableNames(String schema) {
+		this(schema, DEFAULT_TABLE_PREFIX);
+	}
 
-    public String getPrefix() {
-        return prefix;
-    }
+	public TableNames(String schema, String prefix) {
+		Preconditions.checkNotNull(schema);
+		Preconditions.checkNotNull(prefix);
+		this.schema = schema;
+		this.prefix = prefix;
+	}
 
-    public String metadata() {
-        return name("medatada");
-    }
+	public String getPrefix() {
+		return prefix;
+	}
 
-    public String repositories() {
-        return name("repository");
-    }
+	public String metadata() {
+		return name("medatada");
+	}
 
-    public String repositoryNamesView() {
-        return repositories() + "_name";
-    }
+	public String repositories() {
+		return name("repository");
+	}
 
-    private String name(String name) {
-        return new StringBuilder(schema).append('.').append(prefix).append(name).toString();
-    }
+	public String repositoryNamesView() {
+		return repositories() + "_name";
+	}
 
-    public String refs() {
-        return name("ref");
-    }
+	private String name(String name) {
+		return new StringBuilder(schema).append('.').append(prefix).append(name).toString();
+	}
 
-    public String objects() {
-        return name("object");
-    }
+	public String refs() {
+		return name("ref");
+	}
 
-    public String index() {
-        return name("index");
-    }
+	public String objects() {
+		return name("object");
+	}
 
-    public String indexMappings() {
-        return name("index_mappings");
-    }
+	public String index() {
+		return name("index");
+	}
 
-    public String indexObjects() {
-        return name("index_object");
-    }
+	public String indexMappings() {
+		return name("index_mappings");
+	}
 
-    private int featuresTablePartitionCount = 16;
+	public String indexObjects() {
+		return name("index_object");
+	}
 
-    void setFeaturesPartitions(int featuresTablePartitionCount) {
-        Preconditions.checkArgument(
-                featuresTablePartitionCount >= 0 && featuresTablePartitionCount <= 256);
-        this.featuresTablePartitionCount = featuresTablePartitionCount;
-    }
+	private int featuresTablePartitionCount = 16;
 
-    public String features(final int hash) {
-        if (0 == featuresTablePartitionCount) {
-            return features();
-        }
-        final int min = Integer.MIN_VALUE;
-        final long max = (long) Integer.MAX_VALUE + 1;
-        final int numTables = featuresTablePartitionCount;
-        final int step = (int) (((long) max - (long) min) / numTables);
+	void setFeaturesPartitions(int featuresTablePartitionCount) {
+		Preconditions.checkArgument(featuresTablePartitionCount >= 0 && featuresTablePartitionCount <= 256);
+		this.featuresTablePartitionCount = featuresTablePartitionCount;
+	}
 
-        int index = 0;
-        long curr = min;
-        for (int i = 0; i < numTables; i++) {
-            long next = curr + step;
-            if (hash >= curr && hash < next) {
-                index = i;
-                break;
-            }
-            curr = next;
-        }
-        return String.format("%s_%d", features(), index);
-    }
+	public String features(final int hash) {
+		if (0 == featuresTablePartitionCount) {
+			return features();
+		}
+		final int min = Integer.MIN_VALUE;
+		final long max = (long) Integer.MAX_VALUE + 1;
+		final int numTables = featuresTablePartitionCount;
+		final int step = (int) (((long) max - (long) min) / numTables);
 
-    public String features() {
-        return name("object_feature");
-    }
+		int index = 0;
+		long curr = min;
+		for (int i = 0; i < numTables; i++) {
+			long next = curr + step;
+			if (hash >= curr && hash < next) {
+				index = i;
+				break;
+			}
+			curr = next;
+		}
+		return String.format("%s_%d", features(), index);
+	}
 
-    public String featureTypes() {
-        return name("object_featuretype");
-    }
+	public String features() {
+		return name("object_feature");
+	}
 
-    public String tags() {
-        return name("object_tag");
-    }
+	public String featureTypes() {
+		return name("object_featuretype");
+	}
 
-    public String trees() {
-        return name("object_tree");
-    }
+	public String tags() {
+		return name("object_tag");
+	}
 
-    public String commits() {
-        return name("object_commit");
-    }
+	public String trees() {
+		return name("object_tree");
+	}
 
-    public String config() {
-        return name("config");
-    }
+	public String commits() {
+		return name("object_commit");
+	}
 
-    public String conflicts() {
-        return name("conflict");
-    }
+	public String config() {
+		return name("config");
+	}
 
-    public String graphEdges() {
-        return name("graph_edge");
-    }
+	public String conflicts() {
+		return name("conflict");
+	}
 
-    public String graphProperties() {
-        return name("graph_property");
-    }
+	public String graphEdges() {
+		return name("graph_edge");
+	}
 
-    public String graphMappings() {
-        return name("graph_mapping");
-    }
+	public String graphProperties() {
+		return name("graph_property");
+	}
 
-    public String blobs() {
-        return name("blob");
-    }
+	public String graphMappings() {
+		return name("graph_mapping");
+	}
 
-    public List<String> all() {
-        return ImmutableList.of(metadata(), repositories(), config(), refs(), conflicts(),
-                objects(), index(), indexMappings(), indexObjects(), commits(), features(),
-                featureTypes(), trees(), graphEdges(), graphMappings(), graphProperties(), blobs());
-    }
+	public String blobs() {
+		return name("blob");
+	}
+
+	public List<String> all() {
+		return ImmutableList.of(metadata(), repositories(), config(), refs(), conflicts(), objects(), index(),
+				indexMappings(), indexObjects(), commits(), features(), featureTypes(), trees(), graphEdges(),
+				graphMappings(), graphProperties(), blobs());
+	}
 
 }
--- src/storage/postgres/src/main/java/org/locationtech/geogig/storage/postgresql/config/Version.java
+++ src/storage/postgres/src/main/java/org/locationtech/geogig/storage/postgresql/config/Version.java
@@ -9,9 +9,12 @@
  */
 package org.locationtech.geogig.storage.postgresql.config;
 
+import java.util.List;
 import java.util.Objects;
 
 import com.google.common.base.Preconditions;
+import com.google.common.base.Splitter;
+import com.google.common.collect.Lists;
 
 /**
  * Server version, given by it's major, minor, and patch numbers
@@ -21,70 +24,97 @@ import com.google.common.base.Preconditions;
  */
 public class Version implements Comparable<Version> {
 
-    public static final Version V9_4_0 = new Version(9, 4, 0);
+	public static final Version V9_4_0 = new Version(9, 4, 0);
 
-    public static final Version V9_5_0 = new Version(9, 5, 0);
+	public static final Version V9_5_0 = new Version(9, 5, 0);
 
-    public static final Version V9_6_0 = new Version(9, 6, 0);
+	public static final Version V9_6_0 = new Version(9, 6, 0);
 
-    public static final Version V10_0_0 = new Version(10, 0, 0);
+	public static final Version V10_0_0 = new Version(10, 0, 0);
 
-    public final int major;
+	public final int major;
 
-    public final int minor;
+	public final int minor;
 
-    public final int patch;
+	public final int patch;
 
-    Version(int major, int minor, int patch) {
-        this.major = major;
-        this.minor = minor;
-        this.patch = patch;
-    }
+	Version(int major, int minor, int patch) {
+		this.major = major;
+		this.minor = minor;
+		this.patch = patch;
+	}
 
-    public @Override int compareTo(Version o) {
-        int compare = Integer.compare(major, o.major);
-        if (0 == compare) {
-            compare = Integer.compare(minor, o.minor);
-            if (0 == compare) {
-                compare = Integer.compare(patch, o.patch);
-            }
-        }
-        return compare;
-    }
+	public @Override int compareTo(Version o) {
+		int compare = Integer.compare(major, o.major);
+		if (0 == compare) {
+			compare = Integer.compare(minor, o.minor);
+			if (0 == compare) {
+				compare = Integer.compare(patch, o.patch);
+			}
+		}
+		return compare;
+	}
 
-    public @Override boolean equals(Object o) {
-        if (!(o instanceof Version)) {
-            return false;
-        }
-        Version v = (Version) o;
-        return major == v.major && minor == v.minor && patch == v.patch;
-    }
+	public @Override boolean equals(Object o) {
+		if (!(o instanceof Version)) {
+			return false;
+		}
+		Version v = (Version) o;
+		return major == v.major && minor == v.minor && patch == v.patch;
+	}
 
-    public @Override int hashCode() {
-        return Objects.hash(major, minor, patch);
-    }
+	public @Override int hashCode() {
+		return Objects.hash(major, minor, patch);
+	}
 
-    public @Override String toString() {
-        return String.format("%d.%d.%d", major, minor, patch);
-    }
+	public @Override String toString() {
+		return String.format("%d.%d.%d", major, minor, patch);
+	}
 
-    public boolean lowerThan(Version v) {
-        Preconditions.checkNotNull(v);
-        return compareTo(v) < 0;
-    }
+	public boolean lowerThan(Version v) {
+		Preconditions.checkNotNull(v);
+		return compareTo(v) < 0;
+	}
 
-    public boolean lowerOrEqualTo(Version v) {
-        Preconditions.checkNotNull(v);
-        return compareTo(v) <= 0;
-    }
+	public boolean lowerOrEqualTo(Version v) {
+		Preconditions.checkNotNull(v);
+		return compareTo(v) <= 0;
+	}
 
-    public boolean greatherThan(Version v) {
-        Preconditions.checkNotNull(v);
-        return compareTo(v) > 0;
-    }
+	public boolean greatherThan(Version v) {
+		Preconditions.checkNotNull(v);
+		return compareTo(v) > 0;
+	}
 
-    public boolean greatherOrEqualTo(Version v) {
-        Preconditions.checkNotNull(v);
-        return compareTo(v) >= 0;
-    }
+	public boolean greatherOrEqualTo(Version v) {
+		Preconditions.checkNotNull(v);
+		return compareTo(v) >= 0;
+	}
+
+	public static Version valueOf(final String versionQueryResult) {
+		// version string may not be a simple x.y.x value. Let's just take it from the
+		// front and
+		// stop at the first
+		// non-digit, non-decimal-point character
+		final String regex = "[^0-9.]";
+		// replace first non-digit, non-decimal point with XXX
+		String marked = versionQueryResult.replaceFirst(regex, "XXX");
+		if (marked.contains("XXX")) {
+			// no throw away everything starting with XXX
+			marked = marked.substring(0, marked.indexOf("XXX"));
+		}
+		final List<Integer> versions = Lists.transform(Splitter.on('.').splitToList(marked),
+				(s) -> Integer.parseInt(s));
+		// version string can be either
+		// {major}.{minor}.{patch}
+		// or (since PostgreSQL 10)
+		// {major}.{minor}
+		Preconditions.checkState(versions.size() == 3 || versions.size() == 2,
+				"Expected version format x.y.z or x.y, got " + versionQueryResult);
+		int major = versions.get(0).intValue();
+		int minor = versions.get(1).intValue();
+		// patch may not be present. If not, just use 0
+		int patch = (versions.size() == 3) ? versions.get(2).intValue() : 0;
+		return new Version(major, minor, patch);
+	}
 }
